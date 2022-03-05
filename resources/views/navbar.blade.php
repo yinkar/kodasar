ccb6a572803e082d1e2a680d8209d02f449b67b8
Yinkar <hkrabs@yandex.com>
2022-03-05 04:09:20 +0300

    Layout

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,9 +1,9 @@
 
-<nav class="navbar w-fill h-14 bg-neutral-800 shadow-inner border-b-4 border-blue-600 flex items-center justify-between">
+<nav class="navbar w-fill h-14 bg-neutral-800 border-b-4 border-blue-600 flex items-center justify-between">
     <div class="navbar-left flex">
         <div class="logo mx-4">
             <a href="{{ URL::to('/') }}" title="{{ config('app.name') }}">
-                <img src="{{ url('/images/logo.png') }}" alt="{{ config('app.name') }}" />
+                <img src="{{ url('/images/logo.png') }}" alt="{{ config('app.name') }}" width="170" height="25" />
             </a>
         </div>
 
@@ -27,16 +27,16 @@
 
     <div class="navbar-right flex">
         <div class="navbar-icons">
-            <a href="#" class="text-neutral-500 hover:text-neutral-400 transition-colors">
-                <i class="fa-solid fa-square-pen text-3xl"></i>
+            <a href="#" class="bg-neutral-600 hover:bg-neutral-400 text-neutral-800 transition-colors h-8 w-8 flex items-center justify-center rounded-sm">
+                <i class="fa-solid fa-pen text-sm"></i>
             </a>
         </div>
 
-        <div class="search-bar flex mx-4 overflow-hidden rounded-sm">
+        <div class="search-bar flex mx-4 overflow-hidden rounded-sm items-center">
             <div class="input-group relative flex flex-nowrap rounded">
-                <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-neutral-50 bg-neutral-700 bg-clip-padding border-0 transition ease-in-out m-0 focus:text-neutral-100 focus:bg-neutral-600 focus:outline-none placeholder-neutral-500" placeholder="Search" spellcheck="false" />
+                <input type="search" class="form-control relative h-8 flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-neutral-50 bg-neutral-700 bg-clip-padding border-0 transition ease-in-out m-0 focus:text-neutral-100 focus:bg-neutral-600 focus:outline-none placeholder-neutral-500" placeholder="Search" spellcheck="false" />
 
-                <button class="input-group-text flex items-center px-3 py-1.5 text-base font-normal text-neutral-50 bg-neutral-600 text-center whitespace-nowrap hover:bg-neutral-500 transition-colors">
+                <button class="input-group-text flex h-8 items-center px-3 py-1.5 text-base font-normal text-neutral-50 bg-neutral-600 text-center whitespace-nowrap hover:bg-neutral-500 transition-colors">
                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
                     </svg>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,50 @@
 
 <body class="bg-neutral-900">
     @include('navbar');
+
+    <div class="container mx-auto px-4 grid grid-cols-12 gap-3 my-2">
+        <div class="col-span-9">
+            <div class="block bg-neutral-800 text-neutral-100 px-4 py-2 rounded-sm border-neutral-700 border">
+                <h2 class="text-cyan-400 text-3xl mt-2 mb-4">Dictionaries</h2>
+                <ul class="dictionaries">
+                    <li>
+                        <a href="#" class="bg-neutral-700 block px-4 py-2 border-l-4 border-blue-600 hover:border-blue-400 text-lg w-full my-2 hover:bg-neutral-600">
+                            JavaScript
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="bg-neutral-700 block px-4 py-2 border-l-4 border-blue-600 hover:border-blue-400 text-lg w-full my-2 hover:bg-neutral-600">
+                            PHP
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="bg-neutral-700 block px-4 py-2 border-l-4 border-blue-600 hover:border-blue-400 text-lg w-full my-2 hover:bg-neutral-600">
+                            Go
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="bg-neutral-700 block px-4 py-2 border-l-4 border-blue-600 hover:border-blue-400 text-lg w-full my-2 hover:bg-neutral-600">
+                            C
+                        </a>
+                    </li>
+                </ul>
+
+                <div class="my-3 links">
+                    <a href="#" class="text-cyan-500 hover:text-cyan-700 transition-colors">Add language</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-3">
+            @include('sidebar');
+        </div>
+        <div class="info-links">
+            <ul>
+                <li>
+                    <a href="#"></a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </body>
 
 </html>
